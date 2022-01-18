88684a51c6774bc203a403a2a0f60487115c72c9
Jesús Roberto <gamerslive20@gmail.com>
2022-01-18 15:54:00 -0600

    new changes for new advence
    milestone fro major changes

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ let scene = new THREE.Scene();
 // implement particles 
 let  parameters;
 const materials = [];
+let particles;
 
-scene.background = new THREE.Color("rgb(0,0,0)");
-camera = new THREE.PerspectiveCamera(54, window.innerWidth / window.innerHeight, 0.1, 1000);
+scene.background = new THREE.Color("rgb(255,0,0)");
+camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
 //camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 1000 );
 renderer = new THREE.WebGLRenderer({
 });
@@ -18,24 +19,9 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 window.onload = function(){
     let canvas = document.getElementById("welcome");
     canvas.appendChild(renderer.domElement);
+   
+    camera.position.z = -4.5;
     
-    if (window.scrollY / 250.0 < 4.0){
-        camera.position.x = 0.0;
-        camera.position.y = 4.0;//- window.scrollY / 250.0;
-        camera.position.z = 2.0 ;//- window.scrollY / 250.0;
-
-    
-    }else{
-        camera.position.x = 0.0;
-        camera.position.y = 0.1;
-        camera.position.z = -2.0;
-    }
-    
-    /*
-    camera.position.x = 0.0;
-    camera.position.y = 0.1;
-    camera.position.z = -3.5;
-    */
     camera.lookAt(0, 0, 0);
     
     //set Particles 
@@ -64,22 +50,31 @@ window.onload = function(){
         sprs[i].minFilter = THREE.NearestFilter;
     }
 
-
+    /*
     for ( let i = 0; i < 1; i ++ ) {
-        /*
-        const x = 0;
-        const y = 0;
+        
+       // const x = 0;
+       // const y = 0;
+        
         const z = 0;
-        */
+        
         
         const x = Math.random() * 2000 - 1000;
         const y = Math.random() * 2000 - 1000;
-        const z = Math.random() * 2000 - 1000;
+        //const z = Math.random() * 2000 - 1000;
            
         vertices.push( x, y, z );
+        
     }
+    */
+    /*
+    const z = 0;
+    const x = Math.random() * 2000 - 1000;
+    const y = Math.random() * 2000 - 1000;
+    */
 
-    spr_geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+
+    //spr_geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
 
     parameters = [
         /*
@@ -106,16 +101,26 @@ window.onload = function(){
         materials[ i ] = new THREE.PointsMaterial( { size: 1.0, map: sprite, depthTest: false, transparent: true } );
         //materials[ i ].color.setHSL( color[ 0 ], color[ 1 ], color[ 2 ] );
         
-        const particles = new THREE.Points( geometry, materials[ i ] );
+        const inst_particles = new THREE.Points( geometry, materials[ i ] );
        
-        particles.rotation.x = Math.random() * 7;
-		particles.rotation.y = Math.random() * 8;
-		particles.rotation.z = Math.random() * 6;
-        scene.add( particles );
+        //inst_particles.rotation.x = Math.random() * 7;
+		//inst_particles.rotation.y = Math.random() * 8;
+		//inst_particles.rotation.z = Math.random() * 6;
+       //scene.add( inst_particles );
         
     }
 
-   
+    
+    for ( let i = 0; i < scene.children.length; i ++ ){
+        
+        if (scene.children[i] instanceof THREE.Points ){
+            
+            particles = scene.children[i];
+            //particles.prototype.isPoints = true;
+            particles.geometry.computeBoundingBox();
+        }
+    }
+    
 };
 
 // white spotlight shining from the side, casting a shadow
@@ -124,7 +129,7 @@ spotLight.position.set(9, 10, 1);
 scene.add(spotLight);
 
 let gridHelper = new THREE.GridHelper(4, 4);
-scene.add(gridHelper);
+//scene.add(gridHelper);
 
 // example code
 const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -132,12 +137,23 @@ const material = new THREE.MeshStandardMaterial({
     color: 0xff0000
 });
 const topBox = new THREE.Mesh(geometry, material);
-//scene.add(topBox);
+topBox.rotation.y +=5.5;
+scene.add(topBox);
 
 //Sprite
-let pointer = new THREE.Vector2(2.0,2.0);
-let raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+
+let sphereInter;
+let raycaster = new THREE.Raycaster();  
 let INTERSECTED;
+
+const new_geometry = new THREE.SphereGeometry( 0.1 );
+const new_material = new THREE.MeshBasicMaterial( { color: 0x51a8ff  } );
+
+sphereInter = new THREE.Mesh( new_geometry, new_material );
+sphereInter.visible = false;
+scene.add( sphereInter );
+
 
 const map = new THREE.TextureLoader().load('assets/logo_blanco.png');
 map.magFilter = THREE.NearestFilter;
@@ -145,45 +161,53 @@ map.minFilter = THREE.NearestFilter;
 const spr_material = new THREE.SpriteMaterial( { map: map } );
 const sprite = new THREE.Sprite( spr_material );
 sprite.scale.x = 2;
-
+const sprite2 = sprite.clone()
+sprite2.position.y += 1.0   ;
 scene.add( sprite );
+scene.add(sprite2);
 
 window.addEventListener( 'resize', onWindowResize, false );
 onWindowResize();
 
+console.log(scene.children);
 ///// end of example
 let animate = function() {
     
-    
-    
     const time = Date.now() * 0.00005;
+    
     for ( let i = 0; i < scene.children.length; i ++ ) {
 
         const object = scene.children[ i ];
-            
-        if ( object instanceof THREE.Points ) {
+           
+        if ( object instanceof THREE.Mesh ) {
 
-            object.rotation.y = time * ( i < 4 ? i + 1 : - ( i + 1 ) );
-            //console.log("paso")
+            
+            raycaster.setFromCamera( pointer, camera );
+    
+           let intersects = raycaster.intersectObject(object);
+            
+
+           if ( intersects.length > 0 ) {
+           
+                if ( INTERSECTED != intersects[ 0 ].object ) {
+                    
+                    object.rotation.y -= 0.01;
+                    console.log("--")
+                    //console.log(intersects[0].object.geometry)
+                    //console.log(scene.children[i].geometry.vertices)
+                    // console.log("touced");
+                    sphereInter.visible = true;
+					sphereInter.position.copy( intersects[ 0 ].point );
+                }
+           }else if ( INTERSECTED !== null ) {
+       
+               INTERSECTED = null;
+               sphereInter.visible = false;
+           }
+           
         }
     }    
-    
-
-    raycaster.setFromCamera( pointer, camera );
-
-    const intersects = raycaster.intersectObject(sprite);
-    
-    if ( intersects.length > 0 ) {
-
-        if ( INTERSECTED != intersects[ 0 ].object ) {
-            
-            sprite.position.x += .01;   
-        }
-    }else if ( INTERSECTED !== null ) {
-
-        INTERSECTED = null;
-    }
-    
+ 
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 };
@@ -198,7 +222,7 @@ animate();
 
 function updateCamera(ev) {
     
-    if (camera.position.z - window.scrollY / 250.0 > -0.7){
+    if (camera.position.z - window.scrollY / 250.0 > -0.3){
         camera.position.y = 4.0 - window.scrollY / 250.0;
         camera.position.z = 2.0 - window.scrollY / 250.0;
     }
@@ -216,6 +240,6 @@ function move_Particle(){
 
 }
 
-window.addEventListener("scroll", updateCamera);
+//window.addEventListener("scroll", updateCamera);
 document.addEventListener( 'pointermove', onPointerMove );
 
