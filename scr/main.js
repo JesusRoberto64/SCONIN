3b21133f5ee1760f6504e9d6056fdd37072c9770
Jesús Roberto Gzz Fonsca <gamerslive20@gmail.com>
2024-11-21 21:38:28 -0600

    continue wih nav menu

--- scr/main.js
+++ scr/main.js
@@ -24,7 +24,14 @@ $('.carousel').slick({
     
 });
 
+//NAV MENU
+const toogleBtn = document.getElementById("toogle-menu");
+const navMenu = document.getElementById("nav-menu");
 
+toogleBtn.addEventListener('click', ()=>{
+    navMenu.classList.toggle("hidden");
+    navMenu.classList.toggle("visible");
+});
 
 //card new design
 initiCards();
