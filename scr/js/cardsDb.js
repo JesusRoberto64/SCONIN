504ea36e6db6daceaf778e60d90ae221db6a6d64
Jesús Roberto Gzz Fonsca <gamerslive20@gmail.com>
2025-02-23 01:53:09 -0600

    new changes

--- scr/js/cardsDb.js
+++ scr/js/cardsDb.js
@@ -3,19 +3,19 @@ const cardsDb = [
     {
         title: "Jairo: El Pirata Callejero",
         medium: "MINI COMIC IMPRESO",
-        img: "./assets/card_Eliab_01.png",
+        img: "./assets/card_Eliab_06.png",
         url: "https://www.google.com/"
     },
     {
         title: "Protector",
         medium: "MINI JUEGO · 2024",
-        img: "./assets/card_Protector_00.png",
+        img: "./assets/card_Eliab_04.jpg",
         url: "https://sconin.itch.io/protector"
     },
     {
         title: "Eliab y el Círculo del Juego",
         medium: "SERIE DE WEB CÓMIC · 2022",
-        img: "./assets/card_Eliab_00.png",
+        img: "./assets/card_Eliab_06.png",
         url: "https://www.webtoons.com/es/canvas/el-microbusero-2/la-revelaci%C3%B3n/viewer?title_no=742802&episode_no=1"
     },
     
