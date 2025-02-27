950da7e27cfc10a9192913bc7afaa22bcded80c2
Jesús Roberto Gzz Fonsca <gamerslive20@gmail.com>
2025-02-26 23:09:45 -0600

    faq new data format

--- scr/js/faqData.js
+++ scr/js/faqData.js
@@ -5,20 +5,20 @@ const faqs = [
     },
     { 
         question: "¿Por qué no venden su trabajo?",
-        answer: `Porque de hacerlo jamás lo habrías disfrutado. Si quieres que nuestro trabajo sea remunerado ¿para ti cuánto vale? puedes donar aquí.`
+        answer: `<p>Porque de hacerlo jamás lo habrías disfrutado. Si quieres que nuestro trabajo sea remunerado ¿para ti cuánto vale? puedes donar<a class="nav-link" href="apoyo" data-navigo> aquí.</a></p>`
     },
     { 
         question: "¿Qué me recomiendan para hacer obras como ustedes?",
-        answer: "Recomendamos estudiar y practicar a profundidad todos los aspectos que conforman a cada oficio artístico en el que te quieres desempeñar (narrativa, imagen, interactividad, etc.). Estos son algunos libros que consideramos guías importantes: El Arte de la Escritura Dramática. Lajos Egri (1941). Hacer Cómics. Scott McCloud (2012). On Game Design. Chris Crawford (2003)"
+        answer: `<p>Recomendamos estudiar y practicar a profundidad todos los aspectos que conforman a cada oficio artístico en el que te quieres desempeñar (narrativa, imagen, interactividad, etc.). <br>Estos son algunos libros que consideramos guías importantes: <br><br>El Arte de la Escritura Dramática. Lajos Egri (1941). <br>Hacer Cómics. Scott McCloud (2012). <br>On Game Design. Chris Crawford (2003).</p>`
     },
     { 
         question: "¿Puedo colaborar con ustedes?",
-        answer: "Claro. Solo debes contactarnos para charlar sobre la posibilidad de colaborar. Sólo recuerda que somos personas que tienen vidas independientes y que queremos gastar nuestro tiempo en obras que nos parezcan valiosas por su contenido y donde todos estemos dispuestos a trabajar. Considéralo antes de hablarnos."
+        answer: `<p>Claro. Solo debes contactarnos para charlar sobre la posibilidad de colaborar. <br>Sólo recuerda que somos personas que tienen vidas independientes y que queremos gastar nuestro tiempo en obras que nos parezcan valiosas por su contenido y donde todos estemos dispuestos a trabajar. Considéralo antes de hablarnos.</p>`
     },
     {
         question: "¿Cuándo acabará Eliab y el Círculo del Juego?",
         answer: "No tenemos una fecha exacta. Debido a que esta obra consta de muchos episodios hemos decidido alternarla junto a otras obras más pequeñas, por ello su publicación es irregular. Nosotros también esperamos con ansias llegar a su final."
-    }
+    }, 
 ];
 
 export default faqs;
