18e3e280908d4404ffd1f7498a0d6c3386c73f06
JesusRoberto64 <40547350+JesusRoberto64@users.noreply.github.com>
2025-03-19 23:38:14 -0600

    nuevos cambios textos

--- scr/js/faqData.js
+++ scr/js/faqData.js
@@ -1,23 +1,29 @@
 const faqs = [
     { 
         question: "¿Por qué tienen obras gratuitas?",
-        answer: `<p>Consideramos que es la mejor manera para conocer y disfrutar lo que hacemos.<br><br>Si te gusta nuestro trabajo y quieres apoyarnos para seguir creando, puedes donar<a class="nav-link" href="apoyo" data-navigo> aquí.</a></p>`
+        answer: `<p>Consideramos que es la mejor manera para conocer y disfrutar lo que hacemos.<br><br>Si quieres apoyarnos para seguir creando, puedes donar<a class="nav-link" href="apoyo" data-navigo> aquí.</a></p>`
     },
     { 
         question: "¿Cuándo terminará El Microbusero 2?",
-        answer: "<p>No tenemos una fecha exacta.<br><br>Ya que esta obra consta de muchos episodios hemos decidido alternarla junto a otros proyectos más pequeños, así que su publicación es irregular. Nosotros también esperamos con ansias llegar a su final.</p>"
+        answer: "<p>No tenemos una fecha exacta.<br><br>Ya que esta obra consta de muchos episodios hemos decidido alternarla junto a otras obras más pequeñas, así que su publicación es irregular. Nosotros también esperamos con ansias llegar a su final.</p>"
     },
     { 
         question: "¿Puedo colaborar con ustedes?",
-        answer: `<p>Sí, estamos abiertos a colaboraciones. Solo contáctanos para charlar al respecto.<br><br>Eso sí, recuerda que somos personas con vidas independientes y nos gusta invertir nuestro tiempo en proyectos que nos parezcan valiosos y donde todos estemos comprometidos a trabajar. Considéralo antes de escribirnos.</p>`
+        answer: `<p>Sí.<br><br>Eso sí, recuerda que somos personas con vidas independientes y nos gusta invertir nuestro tiempo en proyectos que nos parezcan valiosas. Considéralo antes de escribirnos.</p>`
     },
     { 
-        question: "¿Qué recomiendan para crear obras como las suyas?",
-        answer: `<p>Estudiar y practicar a profundidad los aspectos de cada oficio artístico en el que te quieres desempeñar (narrativa, imagen, interactividad, etc.).<br><br>Estos son algunos libros que consideramos guías importantes:<br><br> <i>El Arte de la Escritura Dramática – Lajos Egri (1941).</i><br><i>Hacer Cómics – Scott McCloud (2012).</i><br><i>On Game Design – Chris Crawford (2003).</i></p>`
+        question: "¿Qué recomiendan para crear obras?",
+        answer: `<p>Estudiar y practicar a profundidad los aspectos de cada oficio artístico en el que te quieres desempeñar (narrativa, imagen, interactividad, etc.).<br><br>Estos son algunos libros que consideramos guías importantes:<br><br></p>
+        <ul>
+            <li><i>El Arte de la Escritura Dramática – Lajos Egri (1941).</i></li>
+            <li><i>Hacer Cómics – Scott McCloud (2012).</i></li>
+            <li><i>On Game Design – Chris Crawford (2003).</i></li>
+        </ul>
+        `
     },
     {
         question: "¿Ofrecen servicios de creación artística?",
-        answer: `<p>Claro, tenemos experiencia haciendo historias, personajes, cómic, animación y videojuegos.<br><br>Si necesitas apoyo en cualquiera de estas áreas, ya sea desde una asesoría hasta la creación de tu proyecto, contáctanos para platicar sobre lo que tienes en mente.</p>`
+        answer: `<p>Claro. tenemos experiencia haciendo historias, personajes, cómic, animación y videojuegos.<br><br>Si necesitas apoyo en cualquiera de estas áreas, ya sea desde una asesoría hasta la creación de tu proyecto, contáctanos para platicar sobre lo que tienes en mente.</p>`
     }, 
 ];
 
