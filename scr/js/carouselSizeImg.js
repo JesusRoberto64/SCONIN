b62fcc15ea796b894975f408663f0dd80b25dbbc
Jesús Roberto Gzz Fonsca <gamerslive20@gmail.com>
2025-04-03 00:22:48 -0600

    nuevo banner sensillo

--- scr/js/carouselSizeImg.js
+++ scr/js/carouselSizeImg.js
@@ -1,35 +1,16 @@
-const largeImgs = [
-    "./assets/banner_escritorio_1.png",
-    "./assets/ANUNCIO_2_ESCRITORIO.png"
-];
+const largeImgs = "./assets/banner_escritorio_1.png"
 
-const smallImgs = [
-    "./assets/banner_movil_1.png",
-    "./assets/ANUNCIO_2_MOVIL.png"
-];
+const smallImgs = "./assets/banner_movil_1.png"
 
-const carouselChangeImg = ()=>{
-    // The number of elements in the arrays above are the overflow images that wil slide in the carousel
-    
-    const banner = document.querySelectorAll('.banner');
-    const width = window.innerWidth;
-
-    if (width <= 576){
-       
-        /*
-        for (let i = 0; i < banner.length; i++) {
-            const element = banner[i];
-            element.src = smallImgs[i%smallImgs.length];
+const carouselChangeImg = () => {
+    const banner = document.getElementById('banner');
+    if (banner != null){
+        const width = window.innerWidth;
+        if (width <= 576){
+            banner.src = smallImgs
+        }else{
+            banner.src = largeImgs
         }
-        */
-    }else{
-        
-        /*
-        for (let i = 0; i < banner.length; i++) {
-            const element = banner[i];
-            element.src = largeImgs[i%largeImgs.length];
-        }
-         */ 
     }
 }
 
